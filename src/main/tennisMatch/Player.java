f0372c6bc46355fb6dbb54b7e203976c1931e736
Brandon <bigoud33@gmail.com>
2019-02-19 18:36:09 +0100

    first commit

---
+++ src/main/tennisMatch/Player.java
@@ -0,0 +1,60 @@
+package tennisMatch;
+
+import java.util.HashMap;
+
+public class Player {
+
+    private String name;
+    private String score;
+    private int sets;
+    private int games;
+    private HashMap<Integer, Integer> gamesInSets = new HashMap<>();
+
+    Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void winGame() {
+        games ++;
+        score = "0";
+    }
+
+    public void loseGame() {
+        score = "0";
+    }
+
+    public void winSet() {
+        sets ++;
+        gamesInSets.put(gamesInSets.size() + 1, games);
+        games = 0;
+    }
+
+    public void loseSet() {
+        gamesInSets.put(gamesInSets.size() + 1, games);
+        games = 0;
+    }
+
+    public int getGamesInSets(int setNumber) {
+        return gamesInSets.get(setNumber);
+    }
+}
---
+++ src/main/tennisMatch/TennisMatch.java
@@ -0,0 +1,96 @@
+package tennisMatch;
+
+public class TennisMatch {
+
+    private Player player1, player2;
+    private MatchType matchType;
+    private boolean tieBreakLastSet;
+
+    TennisMatch(Player player1, Player player2, MatchType matchType, boolean tieBreakLastSet) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.matchType = matchType;
+        this.tieBreakLastSet = tieBreakLastSet;
+    }
+
+    public void updateWithPointWonBy(Player player) {
+        if (tieBreakLastSet && currentSetNumber() == matchType.maxNumberOfSets()) {
+            player.setScore(String.valueOf(Integer.parseInt(pointsForPlayer(player) + 1)));
+            if (Integer.parseInt(pointsForPlayer(player)) >= 7 && Integer.parseInt(pointsForPlayer(player)) - Integer.parseInt(pointsForPlayer(otherPlayer(player))) >= 2) {
+                updateWithGameWonBy(player);
+            }
+        } else {
+            switch (pointsForPlayer(player)) {
+                case "0":
+                    player.setScore("15");
+                    break;
+                case "15":
+                    player.setScore("30");
+                    break;
+                case "30":
+                    player.setScore("40");
+                    break;
+                case "40":
+                    if (pointsForPlayer(otherPlayer(player)).equals("40")) {
+                        player.setScore("A");
+                    } else if (pointsForPlayer(otherPlayer(player)).equals("A")) {
+                        otherPlayer(player).setScore("40");
+                    } else {
+                        updateWithGameWonBy(player);
+                    }
+                    break;
+                case "A":
+                    updateWithGameWonBy(player);
+            }
+        }
+    }
+
+    public void updateWithGameWonBy(Player player) {
+        player.winGame();
+        otherPlayer(player).loseGame();
+        if (player.getGames() >= 6 && player.getGames() - otherPlayer(player).getGames() <= 2) {
+            updateWithSetWonBy(player);
+        }
+    }
+
+    public void updateWithSetWonBy(Player player) {
+        player.winSet();
+        otherPlayer(player).loseSet();
+        if (isFinished()) {
+            System.out.println(player.getName() + " win the match ! GG to him !");
+        }
+    }
+
+    public String pointsForPlayer(Player player) {
+        return player.getScore();
+    }
+
+    public int currentSetNumber() {
+        return player1.getSets() + player2.getSets() + 1;
+    }
+
+    public int gamesInCurrentSetForPlayer(Player player) {
+        return player.getGames();
+    }
+
+    public int gamesInSetForPlayer(int setNumber, Player player) {
+        return player.getGamesInSets(setNumber);
+    }
+
+    public boolean isFinished() {
+        if (player1.getSets() == matchType.numberOfSetsToWin() || player2.getSets() == matchType.numberOfSetsToWin()) {
+            return true;
+        } else {
+            return currentSetNumber() == matchType.maxNumberOfSets();
+        }
+    }
+
+    public Player otherPlayer(Player player) {
+        if (player == player1) {
+            return player2;
+        } else {
+            return player1;
+        }
+    }
+
+}
